09a561efcadaa6cdb9489377243d03432b84931f
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2022-05-13 22:48:21 -0300

    add arquivos

---
+++ sintaxe-variaves-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,19 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Novo Teste");
+		
+		int idade;
+		idade = 31;
+		
+		System.out.println(idade);
+		
+		idade = 12+24;
+		System.out.println(idade);
+		
+		idade = 15;
+		
+		System.out.println("sua idade é " + idade);
+		
+	}	
+}
